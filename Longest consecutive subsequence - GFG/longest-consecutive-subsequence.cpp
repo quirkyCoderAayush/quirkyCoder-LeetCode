922dd53bb2e52d1cbc57e6f4cf218ca37b233c0a
AAYUSH KUMAR <50369625+ucw-web@users.noreply.github.com>
2023-04-04 21:49:44 +0530

    Added solution - LeetHub

--- Longest consecutive subsequence - GFG/longest-consecutive-subsequence.cpp
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.cpp
@@ -12,25 +12,28 @@ class Solution{
     //Function to return length of longest subsequence of consecutive integers.
     int findLongestConseqSubseq(int arr[], int N)
     {
-        int ans=0,count=0;
-        sort(arr, arr+N);
-        vector<int> v;
-        v.push_back(arr[0]);
-        
-        for (int i=1; i<N; i++)
-        {
-            if(arr[i] != arr[i-1])
-                v.push_back(arr[i]);
-        }
-        for (int i=0; i<v.size(); i++)
-        {
-            if (i>0 && v[i]==v[i - 1] + 1)
-                count++;
-            else
-                count=1;
-            ans = max(ans, count);
-        }
-        return ans;
+      //Your code here
+      sort(arr, arr+N);
+      vector<int> v;
+      v.push_back(arr[0]);
+      for(int i=1; i<N; i++)
+      {
+          if(arr[i]!=arr[i-1]) v.push_back(arr[i]);
+      }
+      int count=1, maxi=1;
+      for(int i=1; i<v.size(); i++)
+      {
+          if(v[i] == v[i-1]+1)
+          {
+              count++;
+          }
+          else
+          {
+              count=1;
+          }
+          maxi=max(maxi, count);
+      }
+      return maxi;
     }
 };
 
