9be47c075b33ea88c2098586bed5b2dee49ef3b8
Aayush Kumar <50369625+quirkyCoderAayush@users.noreply.github.com>
2024-12-27 12:22:32 +0530

    Time: 0 ms (100%), Space: 40.6 MB (36.64%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int smaller = Math.min(m - 1, n - 1);
+        int totalSteps = m + n - 2;
+
+        long result = 1;
+
+        for (int i = 1; i <= smaller; i++) {
+            result = result * (totalSteps - i + 1) / i;
+        }
+        return (int) result;
+    }
+}
