19523db8b58e987008877292c1afbf1414ad1ad4
Aayush Kumar <50369625+quirkyCoderAayush@users.noreply.github.com>
2025-01-05 05:49:54 +0530

    Time: 1 ms (99.95%), Space: 45.2 MB (74.5%) - LeetHub

--- 3691-minimum-operations-to-make-columns-strictly-increasing/3691-minimum-operations-to-make-columns-strictly-increasing.java
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/3691-minimum-operations-to-make-columns-strictly-increasing.java
@@ -4,13 +4,12 @@ class Solution {
         int n = grid[0].length;
         int totalOperations=0;
 
-        for(int j=0; j<n; j++) {
-            for(int i=1; i<m; i++) {
-                int prev = grid[i-1][j];
-
-                while(grid[i][j]<=prev) {
-                    grid[i][j] += 1;
-                    totalOperations++;
+        for (int j = 0; j < n; j++) {
+            for (int i = 1; i < m; i++) {
+                int prev = grid[i - 1][j];
+                if (grid[i][j] <= prev) {
+                    totalOperations += prev - grid[i][j] + 1;
+                    grid[i][j] = prev + 1;
                 }
             }
         }
