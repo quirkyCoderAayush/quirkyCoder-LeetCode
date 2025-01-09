71b39319f15d92af2a99b86bd1ef76cb0c2d8dca
Aayush Kumar <50369625+quirkyCoderAayush@users.noreply.github.com>
2025-01-10 00:45:47 +0530

    Time: 2 ms (98.04%), Space: 43.7 MB (62.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,28 +3,14 @@ class Solution {
 
         if (s.length() != t.length()) return false;
 
-        Map<Character, Integer> mpp = new HashMap<>();
+        int[] freq = new int[26];
 
-        for (char ch : s.toCharArray()) {
-            mpp.put(ch, mpp.getOrDefault(ch, 0) + 1);
-        }
+        for(char ch: s.toCharArray()) freq[ch-'a']++;
 
         for(char ch: t.toCharArray()) {
-            if(!mpp.containsKey(ch) || mpp.get(ch)==0) {
-                return false;
-            }
-            else {
-                mpp.put(ch, mpp.get(ch)-1);
-            }
-        }
-        
-        for (int count : mpp.values()) {
-            if (count != 0) {
-                return false;
-            }
+            freq[ch-'a']--;
+            if(freq[ch-'a']<0) return false;
         }
         return true;
-
-
     }
 }
