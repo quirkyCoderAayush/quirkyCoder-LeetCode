c3cd146f537e57665cc64affb6ad56a825609644
Aayush Kumar <50369625+quirkyCoderAayush@users.noreply.github.com>
2025-01-14 02:05:47 +0530

    Time: 15 ms (86.43%), Space: 42.1 MB (92.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,26 @@
 class Solution {
-    int start=0, end=0;
-    int maxLen=0;
-    public boolean checkPalindrome(String s, int i, int j) {
-        while(i<j) {
-            char ch1 = s.charAt(i);
-            char ch2 = s.charAt(j);
-            if(ch1 != ch2) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    public String longestPalindrome(String s) {
-        int n=s.length();
-        if(s==null || n<2) return s;
+    int start = 0, maxLen = 0;
 
-        for(int i=0; i<n; i++) {
-            for(int j=i; j<n; j++) {
-                if(checkPalindrome(s,i,j)) {
-                    if((j-i+1) > maxLen) {
-                        maxLen = j-i+1;
-                        start=i;
-                        end=j;
-                    }
-                }
-            }
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (s == null || n < 2) return s;
+
+        for (int i = 0; i < n; i++) {
+            expandAroundCenter(s, i, i);
+            expandAroundCenter(s, i, i + 1);
         }
-        return s.substring(start, end+1);
+        return s.substring(start, start + maxLen);
+    }
+
+    private void expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            int currentLen = right - left + 1;
+            if (currentLen > maxLen) {
+                maxLen = currentLen;
+                start = left;
+            }
+            left--;
+            right++;
+        }
     }
 }
