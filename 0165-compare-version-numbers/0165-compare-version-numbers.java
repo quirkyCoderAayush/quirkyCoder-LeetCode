046bf4f3fec7077d3fd716ee5dd5c17d22c107b0
Aayush Kumar <50369625+quirkyCoderAayush@users.noreply.github.com>
2025-01-19 22:34:34 +0530

    Time: 1 ms (72.66%), Space: 41.3 MB (93.89%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -2,15 +2,31 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] ver1 = version1.split("\\.");
         String[] ver2 = version2.split("\\.");
+        // ver1 = ["1", "2"]
+        // ver2 = ["1", "10"]
 
         int n = Math.max(ver1.length, ver2.length);
 
         for(int i=0; i<n; i++) {
-            int rev1 = i < ver1.length ? Integer.parseInt(ver1[i]) : 0;
-            int rev2 = i < ver2.length ? Integer.parseInt(ver2[i]) : 0;
+            int rev1, rev2;
 
-            if(rev1<rev2) return -1;
-            if(rev1>rev2) return 1;
+            if (i < ver1.length) {
+                rev1 = Integer.parseInt(ver1[i]);
+            } else {
+                rev1 = 0;
+            }
+
+            if (i < ver2.length) {
+                rev2 = Integer.parseInt(ver2[i]);
+            } else {
+                rev2 = 0;
+            }
+
+            if (rev1 > rev2) {
+                return 1;
+            } else if (rev1 < rev2) {
+                return -1;
+            }
         }
         return 0;
     }
